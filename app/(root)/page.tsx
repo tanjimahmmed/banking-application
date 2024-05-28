99d9be3e3330217b2f3dbcdf04e2355d8534cf6c
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2024-05-28 23:41:51 +0600

    Implement the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,34 @@
+import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = {firstName: 'Tanjim', lastName: 'Adam', email: 'tanjimahmmed.8@gmail.com'};
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+          type="greeting"
+          title="Welcome"
+          user={loggedIn?.firstName || 'Guest'}
+          subtext="Access and manage your account and trasations efficiently"
+          />
+          <TotalBalanceBox 
+          accounts={[]}
+          totalBanks={1}
+          totalCurrentBalance={1250.35}
+          />
+        </header>
+        RECENT TRASACTION
+      </div>
+      <RightSidebar
+      user={loggedIn}
+      transactions={[]}
+      banks={[{currentBalance: 130.50}, {currentBalance: 400.50}]}
+      />
+    </section>
   )
 }
 
