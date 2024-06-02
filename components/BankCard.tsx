c624fa3ac7fffd057480c426e5bebb95a66113ac
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2024-06-02 17:18:41 +0600

    finalize the app

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -2,15 +2,18 @@ import { formatAmount } from '@/lib/utils'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import Copy from './Copy'
 
 const BankCard = ({account, userName, showBalance = true}: CreditCardProps) => {
+    console.log(account);
+    
   return (
     <div className='flex flex-col'>
-        <Link href="/" className='bank-card'>
+        <Link href={`/transaction-history/?id=${account.appwriteItemId}`} className='bank-card'>
             <div className="bank-card_content">
                 <div>
                     <h1 className='text-16 font-semibold text-white'>
-                        {userName}
+                        {account.name}
                     </h1>
                     <p className='font-ibm-plex-serif font-black text-white'>
                         {formatAmount(account.currentBalance)}
@@ -27,7 +30,7 @@ const BankCard = ({account, userName, showBalance = true}: CreditCardProps) => {
                         </h2>
                     </div>
                     <p className='text-14 font-semibold tracking-[1.1px] text-white'>
-                        ●●●● ●●●● ●●●● <span className='text-16'>1234</span>
+                        ●●●● ●●●● ●●●● <span className='text-16'>{account?.mask}</span>
                     </p>
                 </article>
             </div>
@@ -38,6 +41,8 @@ const BankCard = ({account, userName, showBalance = true}: CreditCardProps) => {
 
             <Image src="/icons/lines.png" width={316} height={190} alt="lines" className='absolute top-0 left-0' />
         </Link>
+
+        {showBalance && <Copy title={account?.shareableId} />}
     </div>
   )
 }
